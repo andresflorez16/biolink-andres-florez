e860fd7ff0d5fc7d8eb51d67c013af87f5a6c9f7
andresflorez16 <andresflorez.dev@gmail.com>
2022-11-07 18:15:24 -0500

    finished

---
+++ index.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+	VANTA.BIRDS({
+		el: "#vanta",
+		mouseControls: true,
+		touchControls: true,
+		gyroControls: false,
+		minHeight: 200.00,
+		minWidth: 200.00,
+		scale: 1.00,
+		scaleMobile: 1.00,
+		color1: 0xf79999,
+		separation: 80.00,
+		alignment: 17.00,
+		cohesion: 29.00,
+		quantity: 2.00
+	})
+})
+
+console.log('lñiasdjhf')
